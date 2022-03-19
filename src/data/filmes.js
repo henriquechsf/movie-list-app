b2f860bdf7afae0d15208778f8edc7962ec78ff9
Carlos Henrique de S. Ferreira <ch.ferreir@hotmail.com>
2022-03-19 20:43:17 -0300

    add filmes data

--- src/data/filmes.js
+++ src/data/filmes.js
@@ -1,24 +1,86 @@
 export const filmes = [
   {
     key: 0,
-    filme: 'Batman',
-    ano: 2022,
-    estilo: 'Ação',
-    direcao: 'John Doe',
+    filme: 'Psicose',
+    ano: 1960,
+    estilo: 'Suspense',
+    badge: 'warning',
+    direcao: 'Alfred Hitchcock',
     image:
-      'https://ingresso-a.akamaihd.net/prd/img/movie/batman/15801c12-d376-4fce-a64f-4bdab4a40d4e.jpg',
+      'https://cdn.tlc-massive.com/shain/v1/dataservice/ResizeImage/$value?Format=%27jpg%27&Quality=85&ImageId=%27194950.jpg%27&ImageUrl=%27194950.jpg%27&EntityType=%27Item%27&EntityId=%2713849%27&device=web_browser&subscriptions=Anonymous&Width=360&Height=540',
     descricao:
-      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+      'Após dar um desfalque onde trabalha, a secretária Marion Crane foge da cidade com o dinheiro. Debaixo de uma forte tempestade, ela se hospeda em um decadente hotel na estrada, o Bates Motel. Sem fazer ideia do risco que corre, Marion precisa lidar com Norman, o misterioso dono do local.',
   },
   {
     key: 1,
     filme: 'Spiderman',
-    ano: 2021,
+    ano: 2017,
     estilo: 'Ação',
-    direcao: 'Mark Doe',
+    badge: 'danger',
+    direcao: 'Jon Watts',
     image:
-      'https://terrigen-cdn-dev.marvel.com/content/prod/1x/spider-mannowayhome_lob_crd_03.jpg',
+      'https://cdn.tlc-massive.com/shain/v1/dataservice/ResizeImage/$value?Format=%27jpg%27&Quality=85&ImageId=%27185906.jpeg%27&ImageUrl=%27185906.jpeg%27&EntityType=%27Item%27&EntityId=%2712807%27&device=web_browser&subscriptions=Anonymous&Width=360&Height=540',
     descricao:
-      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+      'Após ajudar os Vingadores, o Homem-Aranha volta à sua rotina no Queens em Nova York, onde passa a combater apenas pequenos crimes locais. Apesar disso, quando o perigoso vilão Abutre surge, o jovem Peter Parker precisa vestir seu traje mais uma vez para salvar a cidade.',
+  },
+  {
+    key: 2,
+    filme: 'Central do Brasil',
+    ano: 1998,
+    estilo: 'Drama',
+    badge: 'success',
+    direcao: 'Walter Sales',
+    image:
+      'https://cdn.tlc-massive.com/shain/v1/dataservice/ResizeImage/$value?Format=%27jpg%27&Quality=85&ImageId=%27207532.jpg%27&ImageUrl=%27207532.jpg%27&EntityType=%27Item%27&EntityId=%2715926%27&device=web_browser&subscriptions=Anonymous&Width=360&Height=540',
+    descricao:
+      'Dora (Fernanda Montenegro) escreve cartas para analfabetos na estação de trem Central do Brasil, no Rio de Janeiro. Após um incidente com uma de suas clientes, ela se sente responsável por Josué (Vinícius de Oliveira), filho da mulher. Juntos, eles acabam embarcando em uma divertida e sensível viagem pelo Brasil, em busca do pai do garoto. Indicado a dois Oscars, incluindo o de Melhor Atriz.',
+  },
+  {
+    key: 3,
+    filme: 'Coringa',
+    ano: 2019,
+    estilo: 'Drama',
+    badge: 'success',
+    direcao: 'Todd Philips',
+    image:
+      'https://cdn.tlc-massive.com/shain/v1/dataservice/ResizeImage/$value?Format=%27jpg%27&Quality=85&ImageId=%27256207.jpg%27&ImageUrl=%27256207.jpg%27&EntityType=%27Item%27&EntityId=%2727252%27&device=web_browser&subscriptions=Anonymous&Width=360&Height=540',
+    descricao:
+      'Arthur Fleck (Joaquin Phoenix) é um aspirante a comediante que, apesar de ter uma condição médica que causa risadas involuntárias, mantém uma vida bem triste. Escorraçado pelas pessoas ao seu redor e pertencente a um cenário familiar complexo, ele assume a identidade de Coringa após matar três homens em um metrô. Vencedor de dois Oscars, incluindo o de Melhor Ator.',
+  },
+  {
+    key: 4,
+    filme: 'Madagascar',
+    ano: 2005,
+    estilo: 'Aninação',
+    badge: 'primary',
+    direcao: 'Eric Darnell',
+    image:
+      'https://cdn.tlc-massive.com/shain/v1/dataservice/ResizeImage/$value?Format=%27jpg%27&Quality=85&ImageId=%27144655.jpeg%27&ImageUrl=%27144655.jpeg%27&EntityType=%27Item%27&EntityId=%274452%27&device=web_browser&subscriptions=Anonymous&Width=540&Height=800',
+    descricao:
+      'Uma zebra (Chris Rock) que sonha com a vida livre e selvagem acaba causando a transferência dela e de seus amigos - um leão (Ben Stiller), uma girafa (David Schwimmer) e um hipopótamo (Jada Pinkett Smith) - do zoológico de Nova York para uma reserva na África. Acidentalmente, o grupo acaba em uma ilha totalmente selvagem, onde a vida é bem diferente do que esperavam.',
+  },
+  {
+    key: 5,
+    filme: 'O Exterminador',
+    ano: 1984,
+    estilo: 'Ação',
+    badge: 'danger',
+    direcao: 'James Cameron',
+    image:
+      'https://cdn.tlc-massive.com/shain/v1/dataservice/ResizeImage/$value?Format=%27jpg%27&Quality=85&ImageId=%27219588.jpg%27&ImageUrl=%27219588.jpg%27&EntityType=%27Item%27&EntityId=%277159%27&device=web_browser&subscriptions=Anonymous&Width=360&Height=540',
+    descricao:
+      'Num futuro dominado pelas máquinas, os computadores mandam ao passado um androide (Arnold Schwarzenegger) praticamente indestrutível. Sua missão é matar Sarah Connor antes que ela dê à luz o líder da resistência humana na guerra contra a inteligência artificial. Os seres humanos também enviam um agente para proteger a mulher.',
+  },
+  {
+    key: 6,
+    filme: 'Batman',
+    ano: 2022,
+    estilo: 'Ação',
+    badge: 'danger',
+    direcao: 'Justin Copeland',
+    image:
+      'https://ingresso-a.akamaihd.net/prd/img/movie/batman/15801c12-d376-4fce-a64f-4bdab4a40d4e.jpg',
+    descricao:
+      'Um novo inimigo tenta destruir a carreira de Batman aliando-se aos demais vilões de Gotham City. Em meio ao caos, o herói também enfrenta problemas na vida pessoal.',
   },
 ];
